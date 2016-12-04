9d4b7a2713a98ba48731a779717ea8385cd95598
Bruno Paulo <bruno@werules.com>
2016-12-04 12:59:27 -0200

    initial commit

---
+++ Assets/Scripts/MovableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovableObject : MonoBehaviour {
+
+    public int speed = 1;
+    [SerializeField] float resetXPosition;
+    [SerializeField] float restartXPosition;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (transform.localPosition.x == 
+        transform.Translate(Vector3.left * Time.deltaTime * this.speed);
+
+        if (transform.localPosition.x <= resetXPosition)
+            transform.position = new Vector3(restartXPosition, transform.localPosition.y, transform.localPosition.z);
+	}
+}
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : MovableObject {
+
+    [SerializeField] Vector3 topPosition;
+    [SerializeField] Vector3 bottomPosition;
+    public int speed;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(MoveRock(bottomPosition));
+	}
+
+    IEnumerator MoveRock(Vector3 target)
+    {
+        while (Mathf.Abs((target - transform.localPosition).y) > 0.5f)
+        {
+            Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+            transform.localPosition += direction * Time.deltaTime * this.speed;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+
+        StartCoroutine(MoveRock(newTarget));
+    }
+}
