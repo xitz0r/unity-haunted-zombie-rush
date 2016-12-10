f6f54f6197a3829b822e67d2d95474ad40f0d1a4
Bruno Paulo <bruno@werules.com>
2016-12-10 12:49:22 -0200

    making zombie player animation jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        this.animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            animator.Play("Jump");
+	}
+}
