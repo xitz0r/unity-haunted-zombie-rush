c472a1217c2cb8465d589421611aef125586b667
Bruno Paulo <bruno@werules.com>
2016-12-11 09:40:56 -0200

    adding physics to rocks and zombie [ex 26]

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,33 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     private Animator animator;
+    private bool jump;
+    private Rigidbody rigidbody;
 
 	// Use this for initialization
 	void Start () {
         this.animator = GetComponent<Animator>();
+        this.jump = false;
+        this.rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0))
+        {
             animator.Play("Jump");
+            this.jump = true;
+            this.rigidbody.useGravity = true;
+        }
 	}
+
+    void FixedUpdate()
+    {
+        if (this.jump)
+        {
+            this.jump = false;
+            this.rigidbody.velocity = Vector3.zero;
+            this.rigidbody.AddForce(100f * Vector3.up, ForceMode.Impulse);
+        }
+    }
 }
